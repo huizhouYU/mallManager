cc34db315ff38cf05730ff48d90d9b9807756ea1
huizhouYU <931983091@qq.com>
2022-08-08 17:00:19 +0800

    登录页加上验证码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -3,11 +3,18 @@
     <div class="login-box">
       <el-form label-position="top" label-width="80px" :model="loginForm" class="login-form">
         <h2>登录</h2>
-        <el-form-item label="用户名">
+        <el-form-item label="用户名" class="form-label">
           <el-input v-model="loginForm.username" clearable></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码"  class="form-label">
           <el-input v-model="loginForm.password" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="验证码" class="form-label">
+          <div class="login-code">
+            <el-input v-model="loginForm.verCode" class="login-code-input"></el-input>
+            <div class="code" @click="createCode()" ></div>
+            <a href="#" @click="createCode()">看不清换一张</a>
+          </div>
         </el-form-item>
         <el-button type="primary" round class="login-btn" @click="login">登录</el-button>
       </el-form>
@@ -21,14 +28,56 @@
       return {
         loginForm: {
           username: '',
-          password: ''
-        }
+          password: '',
+          verCode: ''
+        },
+        code: '', //验证码的初始值
       }
     },
+    mounted() {
+      this.createCode()
+    },
     methods: {
+      //生成验证码
+      createCode() {
+        this.code = ""; //验证码的初始值
+        const codeLength = 4; //验证码的长度
+        // 验证码的组成成分
+        var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f',
+          'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+          'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+          'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+        for (var i = 0; i < codeLength; i++) {
+          // 获取随机数：下标
+          var charNum = Math.floor(Math.random() * 62);
+          // 验证码
+          this.code += codeChars[charNum];
+        }
+        document.getElementsByClassName("code")[0].innerText = this.code;
+      },
+      // 验证验证码的函数
+      checkCode() {
+        // 判断验证码是否输入正确
+        if (this.loginForm.verCode.length<=0){
+          this.$message.error('请输入验证码！')
+          return false
+        }else if(this.loginForm.verCode.toUpperCase() != this.code.toUpperCase()){
+          this.$message.error('验证码输入有误！')
+          // 更新验证码
+          this.createCode();
+          // 清空输入框
+         this.loginForm.verCode = "";
+         return false
+        }else{
+          return true
+        }
+      },
       login() {
         if (this.loginForm.username === '' && this.loginForm.password === '') {
           this.$message.error('用户名和密码不能为空！')
+          return ''
+        }
+        if(!this.checkCode()){
           return ''
         }
         // this.$http.post('login', this.loginForm).then(res => {
@@ -43,7 +92,7 @@
         //   }
         // })
         //假设用户名和密码正确
-        localStorage.setItem('token', '11111') //保存一个token值
+        // localStorage.setItem('token', '11111') //保存一个token值
         // 登录成功 跳转首页
         this.$router.push({
           name: 'home'
@@ -106,7 +155,48 @@
 
   .login-btn {
     width: 100%;
-    margin-bottom: 10px;
+    margin:20px 0px 10px 0px;
     letter-spacing: 2px;
+  }
+
+  .code {
+    width: 100px;
+    height: 40px;
+    background-color: #3c8be6;
+    display: inline-block;
+    text-align: center;
+    line-height: 40px;
+    letter-spacing: 10px;
+    font-style: italic;
+    /*使鼠标放在上面的时候将鼠标的图标变成一个小手*/
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: #ff905c;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .login-code{
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+  }
+
+  .login-code-input{
+    margin-right: 10px;
+    width: 160px;
+  }
+
+  .el-form-item{
+    margin-bottom: 0rem;
+  }
+
+  .form-label /deep/ .el-form-item__label{
+    padding: 0px;
   }
 </style>
